3e164ba64d877d4ff2a022290cd87f44eb1d079d
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-09 16:29:02 +0900

    16진수
    BOJ 1550: 16진수

---
+++ BOJ/Bronze/_1550.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class _1550 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		String c = sc.next();
+		int binaryToHex = Integer.parseInt(c, 16);
+		System.out.println(binaryToHex);
+		
+	}	
+}
