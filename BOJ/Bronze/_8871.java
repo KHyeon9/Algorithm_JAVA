673781b3890335b75795bbd6b2ae8ff2d5b2b404
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-11 20:15:14 +0900

    Zadanie próbne 2
    BOJ 8871: Zadanie próbne 2

---
+++ BOJ/Bronze/_8871.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class _8871 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(String.format("%d %d", 2 * n + 2, 3 * n + 3));
+ 	}	
+}
