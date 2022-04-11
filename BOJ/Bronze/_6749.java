2e543b460120b632b6b65b0ee84bbd006b68d020
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-11 19:50:14 +0900

    Next in line
    BOJ 6749: Next in line

---
+++ BOJ/Bronze/_6749.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class _6749 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int kid1 = sc.nextInt();
+		int kid2 = sc.nextInt();
+		System.out.println(kid2 * 2 - kid1);
+
+ 	}	
+}
