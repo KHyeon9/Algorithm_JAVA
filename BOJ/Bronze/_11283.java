b3f0986f78f013841ee6be4e86a78340a4e7135f
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-12 20:16:22 +0900

    한글 2
    BOJ 11283: 한글 2

---
+++ BOJ/Bronze/_11283.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _11283 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        int num = (int) ch;
+        System.out.println(num - 44031);
+    }
+}
