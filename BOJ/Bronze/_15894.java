6b6be294ad6a2286c03d9a136f631aa27081df15
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-13 20:32:26 +0900

    수학은 체육과목 입니다
    BOJ 15894: 수학은 체육과목 입니다

---
+++ BOJ/Bronze/_15894.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class _15894 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger n = scanner.nextBigInteger();
+
+        System.out.println(n.multiply(BigInteger.valueOf(4)));
+    }
+}
