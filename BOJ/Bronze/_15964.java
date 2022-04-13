de56f9efd0d25c8f5b20699674ea105816a0f858
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-13 20:38:43 +0900

    이상한 기호
    BOJ 15964: 이상한 기호

---
+++ BOJ/Bronze/_15964.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class _15964 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextInt();
+        long b = scanner.nextInt();
+
+        System.out.println((a + b) * (a - b));
+    }
+}
