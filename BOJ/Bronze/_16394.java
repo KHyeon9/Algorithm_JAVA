d9a9ec9efa7675f7e3af7a7aa1b39a4a852a1a1a
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-13 20:44:04 +0900

    홍익대학교
    BOJ 16394: 홍익대학교

---
+++ BOJ/Bronze/_16394.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _16394 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long year = scanner.nextInt();
+
+        System.out.println(year - 1946);
+    }
+}
