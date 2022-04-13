4fc3b4e825da2c10848d6c93e3fbc2ae2bcc8877
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-13 19:55:23 +0900

    와이버스 부릉부릉
    BOJ 14645: 와이버스 부릉부릉

---
+++ BOJ/Bronze/_14645.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class _14645 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int a, b;
+        for (int i=0; i<n; i++){
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
