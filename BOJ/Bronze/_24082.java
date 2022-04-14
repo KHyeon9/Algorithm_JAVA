3652dbc3c6f92ffa39ddf5846ec651fc85581053
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-14 20:36:55 +0900

    立方体 (Cube)
    BOJ 24082: 立方体 (Cube)

---
+++ BOJ/Bronze/_24082.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class _24082 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        System.out.println((int) Math.pow(n, 3));
+
+    }
+}
