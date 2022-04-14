1a3d6093eb2a762b09a5c69fa1f193a7e9bae2f8
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-14 19:46:14 +0900

    제리와 톰
    BOJ 16430: 제리와 톰

---
+++ BOJ/Bronze/_16430.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class _16430 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(String.format("%d %d", b - a, b));
+    }
+}
