49256ecd647aa30b9181bc1f83e44e06acdd39a9
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-14 20:03:22 +0900

    Арифметическая магия
    BOJ 18096: Арифметическая магия

---
+++ BOJ/Bronze/_18096.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class _18096 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        System.out.println(1);
+    }
+}
