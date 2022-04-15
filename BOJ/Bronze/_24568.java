d3933d7412e4d56134cfa65f7ae7f9141206203d
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-15 20:51:45 +0900

    Cupcake Party
    BOJ 24568: Cupcake Party

---
+++ BOJ/Bronze/_24568.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class _24568 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int s = sc.nextInt();
+
+        System.out.println(r * 8 + s * 3 - 28);
+    }
+}
