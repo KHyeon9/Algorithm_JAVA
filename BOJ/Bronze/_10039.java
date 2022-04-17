ddca25cf5c71ac4198c69f02bd2e903ffb4c9e25
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-04-17 20:05:50 +0900

    평균 점수
    BOJ 10039: 평균 점수

---
+++ BOJ/Bronze/_10039.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class _10039 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int n = sc.nextInt();
+            if (n < 40) {
+                n = 40;
+            }
+            sum += n;
+        }
+
+        System.out.println(sum / 5);
+    }
+}
