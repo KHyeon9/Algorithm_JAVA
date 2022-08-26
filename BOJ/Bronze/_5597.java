041f60440c1c6c75b2c0a639594d8b9b7bf08e7d
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2022-08-26 11:06:47 +0900

    과제 안 내신 분..?
    BOJ 5597: 과제 안 내신 분..?

---
+++ BOJ/Bronze/_5597.java
@@ -0,0 +1,22 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+
+public class _5597 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[31];
+		for (int i = 0; i < 28; i++) {
+			int n = sc.nextInt();
+			arr[n] = 1;
+		}
+		
+		for (int j = 1; j < 31; j++) {
+			if (arr[j] == 0) {
+				System.out.println(j);
+			}
+		}
+	}
+
+}
