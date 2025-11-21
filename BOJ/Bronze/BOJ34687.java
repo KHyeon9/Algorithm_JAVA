3cc9cb7f77c43a7d4654cadfe989a562b9c13ead
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2025-11-21 14:19:22 +0900

    라면 끓여 먹자 야호
    BOJ 34687: 라면 끓여 먹자 야호

---
+++ BOJ/Bronze/BOJ34687.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ34687 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(m * 100 >= n * 81 ? "yaho" : "no");
+    }
+}
