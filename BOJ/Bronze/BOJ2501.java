cd795636c1736be2067575278217410fc7e56680
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2025-12-21 20:32:47 +0900

    약수 구하기
    BOJ 2501: 약수 구하기

---
+++ BOJ/Bronze/BOJ2501.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BOJ2501 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int cnt = 0, answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                cnt++;
+                if (cnt == k) {
+                    answer = i;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
