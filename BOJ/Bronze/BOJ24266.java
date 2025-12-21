fcc68d8f2b795551aa51a232b5226607404b6c4f
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2025-12-21 21:46:39 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 5
    BOJ 24266: 알고리즘 수업 - 알고리즘의 수행 시간 5

---
+++ BOJ/Bronze/BOJ24266.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ24266 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+
+        System.out.println(n * n * n);
+        System.out.println(3);
+    }
+}
