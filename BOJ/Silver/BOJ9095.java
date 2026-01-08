33037a61132350ace64fcd1bde4df541daa18c36
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2026-01-08 22:56:03 +0900

    1, 2, 3 더하기
    BOJ 9095: 1, 2, 3 더하기

---
+++ BOJ/Silver/BOJ9095.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BOJ9095 {
+    static int[] dp = new int[11];
+    static {
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        dp[4] = 7;
+
+        for (int i = 5; i < 11; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n  = sc.nextInt();
+            System.out.println(dp[n]);
+        }
+    }
+}
