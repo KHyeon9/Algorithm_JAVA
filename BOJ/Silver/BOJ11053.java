9ef77498ff50a7743853f95e2f4e3096ccfc58d8
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2026-01-13 16:05:12 +0900

    가장 긴 증가하는 부분 수열
    BOJ 11053: 가장 긴 증가하는 부분 수열

---
+++ BOJ/Silver/BOJ11053.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        // LIS 알고리즘
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int answer = Arrays.stream(dp).max().getAsInt();
+        System.out.println(answer);
+    }
+}
