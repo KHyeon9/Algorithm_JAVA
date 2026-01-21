378d11e9480e89039d7f67d2ebc68a8c2255ac22
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2026-01-21 16:19:57 +0900

    가희와 철도역
    BOJ 34667: 가희와 철도역

---
+++ BOJ/Bronze/BOJ34667.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BOJ34667 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String t = sc.next();
+
+        System.out.println(s.charAt(0) + s);
+    }
+}
