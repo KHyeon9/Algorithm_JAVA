12b363669108235d1d288f92bc1aea9e673ccc5c
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2026-02-13 15:31:48 +0900

    Circular Shortcut
    BOJ 35071: Circular Shortcut

---
+++ BOJ/Bronze/BOJ35071.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class BOJ35071 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double d = sc.nextInt();
+
+        System.out.println((Math.PI * d) / 2 - d);
+    }
+}
