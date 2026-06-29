409cc7c7823ca0d95d1551950aa91405dccba794
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2026-06-29 09:50:55 +0900

    Fibonacci Number
    LeetCode509: Fibonacci Number

---
+++ LeetCode/LeetCode509.java
@@ -0,0 +1,10 @@
+public class LeetCode509 {
+    public int fib(int n) {
+        int[] dp = new int[31];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+}
