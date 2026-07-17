7b2b915136732b75579988c3fa8ccdcb3e7bfd00
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2026-07-17 17:05:46 +0900

    Power of Two
    LeetCode389: Power of Two

--- LeetCode/LeetCode389.java
+++ LeetCode/LeetCode389.java
@@ -1,21 +1,12 @@
-import java.util.*;
-
 public class LeetCode389 {
-    // Find the Difference
-    public char findTheDifference(String s, String t) {
-        // 문자의 갯수 담기
-        int[] alpaCnt =  new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            alpaCnt[s.charAt(i) - 'a']++;
+    // Power of Two
+    public boolean isPowerOfTwo(int n) {
+        // 0이하 확인
+        if (n <= 0) return false;
+        // 2로 계속 나누어서 확인
+        while (n % 2 == 0) {
+            n /= 2;
         }
-        // 다른 문자열의 문자 갯수 빼기
-        for (char c : t.toCharArray()) {
-            alpaCnt[c - 'a']--;
-        }
-        // 계산
-        for (int i = 0; i < 26; i++) {
-            if (alpaCnt[i] != 0) return (char)(i + 'a');
-        }
-        return 0;
+        return n == 1;
     }
 }
