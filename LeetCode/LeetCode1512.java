55bb83149cda45569d2f74642af829e2fb0e48b8
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2026-08-12 20:47:18 +0900

    Number of Good Pairs
    LeetCode1512: Number of Good Pairs

---
+++ LeetCode/LeetCode1512.java
@@ -0,0 +1,15 @@
+public class LeetCode1512 {
+    // Number of Good Pairs
+    public int numIdenticalPairs(int[] nums) {
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
