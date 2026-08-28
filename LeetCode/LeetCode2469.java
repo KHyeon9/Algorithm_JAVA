ca3bfc18f7483ba0a083dfa84a72cc87080d31c5
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2026-08-28 22:11:54 +0900

    Convert the Temperature
    LeetCode2469: Convert the Temperature

---
+++ LeetCode/LeetCode2469.java
@@ -0,0 +1,7 @@
+public class LeetCode2469 {
+    // Convert the Temperature
+    public double[] convertTemperature(double celsius) {
+        return new double[] {celsius + 273.15, celsius * 1.8 + 32};
+    }
+}
+
