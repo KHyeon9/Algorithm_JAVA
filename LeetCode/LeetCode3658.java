6d1db4d56bc8150b7d77ffc8f2e6e405c02247f7
KHyeon9 <92162623+KHyeon9@users.noreply.github.com>
2026-09-01 21:01:00 +0900

    GCD of Odd and Even Sums
    LeetCode3658: GCD of Odd and Even Sums

---
+++ LeetCode/LeetCode3658.java
@@ -0,0 +1,14 @@
+public class LeetCode3658 {
+    // GCD of Odd and Even Sums
+    public int gcdOfOddEvenSums(int n) {
+        int sumOdd = n * n;
+        int sumEven = n * (n + 1);
+
+        return gcd(sumOdd, sumEven);
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
+
